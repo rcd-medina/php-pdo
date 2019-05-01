8070dde1448cdb1eba07d8e1f50dc22e352af4e7
Ricardo Medina <ricardo.medina.dev@gmail.com>
2019-04-30 21:44:50 -0300

    Criada a classe User no arquivo app/models/User.php. A classe no momento não possui implementação, apenas herda a classe base Model.

---
+++ app/models/User.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\models;
+
+class User extends Model
+{
+
+    /*public function all()
+    {
+        # code...
+    }
+
+    public function find()
+    {
+        # code...
+    }*/
+}
+
+
