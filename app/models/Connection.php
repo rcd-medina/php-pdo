3232d341d4968e9883a8535c9a4b95195b944a8c
Ricardo Medina <ricardo.medina.dev@gmail.com>
2019-04-30 21:34:14 -0300

    Criado a classe Connection no arquivo app/models/Connection.php para a execução de conxão com o banco de dados. A classe possui, no momento, um método estático que irá acessar os dados do arquivo config.php que mantém as informações para conexão com o banco de dados, hostname, dbname, username... etc.

---
+++ app/models/Connection.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\models;
+
+class Connection
+{
+    public static function connect()
+    {
+        $config = require "../config.php";
+        // ================================================================================================
+        // Configura a conexão com o banco de dados.
+        // ================================================================================================
+        $pdo = new PDO("mysql:host={$config['host']};dbname = {$config['dbname']};charset={$config['charset']}", $config['username'], $config['password']);
+
+        // ================================================================================================
+        // Configura o modo de erro a ser exibido caso ocorra algum problema com o PDO.
+        // ================================================================================================
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // ================================================================================================
+        // Configura o PDO para tratar os dados no formato de objetos.
+        // ================================================================================================
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+        // ================================================================================================
+        // Retorna o objeto PDO criado.
+        // ================================================================================================
+        return $pdo;
+    }
+}
+
+
