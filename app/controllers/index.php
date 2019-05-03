f44c5dfe4570e5f4b2339a9b2695ec2877c408e1
Ricardo Medina <ricardo.medina.dev@gmail.com>
2019-05-02 22:41:40 -0300

    Alterado código para execução de testes.

--- app/controllers/index.php
+++ app/controllers/index.php
@@ -3,9 +3,10 @@
 use app\models\User;
 
 $user = new User();
-$users = $user->all();
+//$users = $user->all();
+$userEncontrado = $user->find('id', 3);
 
-dd($users);
+dd($userEncontrado);
 
 require "../app/views/index.php";
 
