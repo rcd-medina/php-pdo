79eb515b0f3a2487dd2e433c58a12127fe5b16cf
Ricardo Medina <ricardo.medina.dev@gmail.com>
2019-05-02 21:50:01 -0300

    Adicionado comentários para exemplificar o uso e diferenças entre os métodos prepare() e query() de um objeto PDO. Também comentários sobre a diferença entre fetch() e fetchAll(), métodos do objeto PDOStatement.

--- app/models/Model.php
+++ app/models/Model.php
@@ -18,14 +18,42 @@ abstract class Model
         // Criação da sentença SQL.
         // ============================================================================================
         $sql = "SELECT * FROM {$this->table}";
+
+        // ============================================================================================
+        // O método prepare() do objeto connection, que é a conexão PDO, retorna um objeto do tipo
+        // PDOStatement, por esse motivo é bem comum declararem a variável $stmt ao invés de $list,
+        // como feito neste exemplo.
+        // O método query() também retorna um PDOStatement, com algumas diferenças.
+        // O método prepare() prepara uma sentença SQL permitindo a utilização de um placeholder que
+        // depois será trocado por um valor real. O método query() não permite isso.
+        // O método prepare() já nos previne contra ataques do tipo SQL Injection, ou seja, é mais
+        // seguro do que utilizar o método query().
+        //
+        // Ex.:
+        // $sql = "SELECT * FROM {$this->table} WHERE id = :placeholder";
+        //
+        // O placeholder será substituído quando ocorrer a chamada ao método bindValue() do objeto
+        // $stmt, neste exemplo o $list. O primeiro parâmetro passado é o nome do placeholder a ser
+        // substituído, o segundo parâmetro é o valor real ou variável com o valor.
+        // No exemplo estamos trocando o placeholder pelo valor 2
+        //
+        // Ex.:
+        // $list->bindValue(':placeholder', 2)
+        // ============================================================================================
         $list = $this->connection->prepare($sql);
+        $list->bindValue(':id', 1);
         $list->execute();
 
         // ============================================================================================
         // Retorna um array multidimensional em que cada índice aponta para outro array que contém
         // os dados de um registro da tabela.
-        // Ou seja, a consulta retornar 3 registros, no array multidimensional haveram mais 3 arrays
+        // Ou seja, se a consulta retornar 3 registros, no array multidimensional haveram mais 3 arrays
         // sendo que cada um é um registro (linha) da tabela.
+        //
+        // O método fetchAll() do objeto PDOStatement retorna todos os registros encontrados no banco
+        // de dados quando o critério de busca, dado, é genérico.
+        //
+        // O método fetch() do objeto PDOSstatement retorna apenas 1 registro.
         // ============================================================================================
         return $list->fetchAll();
     }
