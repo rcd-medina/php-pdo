bc39f3a845609ac0d9d5e705b26b2ef7adc4e7e4
Ricardo Medina <ricardo.medina.dev@gmail.com>
2019-05-02 21:47:36 -0300

    Adicionado código para uma mlehor formatação de saída para diagnóstico de dados.

--- app/functions/helpers.php
+++ app/functions/helpers.php
@@ -2,7 +2,16 @@
 
 function dd($dump)
 {
+    echo "<h2>Para diagnóstico</h2><br>";
+    echo "<h4>var_dump()</h4>";
+    echo "<pre>";
     var_dump($dump);
+    echo "</pre><br><br>";
+
+    echo "<h4>print_r()</h4>";
+    echo "<pre>";
+    print_r($dump);
+    echo "</pre><br><br>";
     die();
 }
 
